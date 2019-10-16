9b89cb290c15d20a266bc64746bd54fa8e7f1b90
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-10-16 12:59:28 -0700

    updated pom file for dependencies

--- school/src/main/java/com/lambdaschool/school/config/Swagger2Config.java
+++ school/src/main/java/com/lambdaschool/school/config/Swagger2Config.java
@@ -23,7 +23,9 @@ public class Swagger2Config
                 .select()
                 .apis(RequestHandlerSelectors
                         .basePackage("com.lambdaschool.school"))
-                .paths(PathSelectors.any()).build()
+                .paths(PathSelectors.any())
+                //  .paths(PathSelectors.regex("/**))
+                .build()
                 .useDefaultResponseMessages(false) // Allows only my exception responses
                 .ignoredParameterTypes(Pageable.class) // allows only my paging parameter list
                 .apiInfo(apiEndPointsInfo());
